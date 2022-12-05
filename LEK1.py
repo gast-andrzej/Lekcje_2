21d181cc9030a0bb2c2078260013d3bd1bc09172
AG <gastandrzej@gmail.com>
2022-12-05 12:27:17 +0100

    lek

--- LEK1.py
+++ LEK1.py
@@ -89,3 +89,81 @@ prawie identyczny z rozkładem poissona, tak że n * p jest prawie równe lam.
 # seab.distplot(np.random.poisson(lam=10, size=1000), hist=False, label='poisson')
 #
 # pypl.show()
+
+
+'''
+Jednolita dystrybucja
+Używane do opisania prawdopodobieństwa, w którym każde zdarzenie ma równe szanse wystąpienia.
+
+Np. Generowanie liczb losowych.
+
+Ma trzy parametry:
+
+a - dolna granica - domyślnie 0 .0.
+
+b - górna granica - domyślnie 1.0.
+
+size — Kształt zwracanej tablicy.
+
+'''
+
+# x = np.random.uniform(size=(2, 3))
+#
+# print(x)
+
+
+
+# WIZUALIZACJA
+
+#
+# seab.distplot(np.random.uniform(size=1000), hist=True)
+# seab.distplot(np.random.uniform(low=0,high=150,size=5))
+# pypl.show()
+
+'''
+Dystrybucja logistyczna
+Dystrybucja logistyczna jest używana do opisania wzrostu.
+
+Szeroko stosowany w uczeniu maszynowym w regresji logistycznej, sieciach neuronowych itp.
+
+Ma trzy parametry:
+
+loc - średnia, gdzie jest szczyt. Domyślnie 0.
+
+scale - odchylenie standardowe, płaskość rozkładu. Domyślnie 1.
+
+size — Kształt zwracanej tablicy.
+
+'''
+
+# x = np.random.logistic(loc=0, scale=3, size=(2, 2))
+#
+# print(x)
+
+
+# WIZUALIZACJA
+
+# seab.distplot(np.random.logistic(loc=0,scale=10,size=1000), hist=False)
+# seab.distplot(np.random.logistic(size=1000), hist=True)
+#
+# pypl.show()
+
+'''
+Różnica między dystrybucją logistyczną a normalną
+Oba rozkłady są prawie identyczne, ale rozkład logistyczny ma większy obszar pod ogonami, co oznacza, że 
+reprezentuje większe prawdopodobieństwo wystąpienia zdarzenia dalej od średniej.
+
+Dla wyższej wartości skali (odchylenie standardowe) rozkłady normalny 
+i logistyczny są prawie identyczne poza szczytem.
+
+'''
+
+# WIZUALIZACJA
+
+# seab.distplot(np.random.normal(scale=2, size=1000), hist=True, label='normal')
+# seab.distplot(np.random.logistic(size=1000), hist=True, label='logistic')
+#
+# seab.distplot(np.random.normal(scale=2, size=1000), hist=False, label='normal')
+# seab.distplot(np.random.logistic(size=1000), hist=False, label='logistic')
+
+# pypl.show()
